624c857f4e15954573ec5e56485706f91bed28c5
lsh58 <tmdgusdl58@gmail.com>
2020-05-18 10:28:42 +0900

    .

--- js/script.js
+++ js/script.js
@@ -5,6 +5,46 @@ $(function () {
 
     $('.menu').click(menuOpen);
 
+    $('.menuOpen__home').mouseenter(function () {
+        $('.menuSelect__home').css({
+            top: 0
+        })
+    });
+    $('.menuOpen__home').mouseleave(function () {
+        $('.menuSelect__home').css({
+            top: '-100%'
+        })
+    });
+    $('.menuOpen__about').mouseenter(function () {
+        $('.menuSelect__about').css({
+            top: 0
+        })
+    });
+    $('.menuOpen__about').mouseleave(function () {
+        $('.menuSelect__about').css({
+            top: '-100%'
+        })
+    });
+    $('.menuOpen__project').mouseenter(function () {
+        $('.menuSelect__project').css({
+            top: 0
+        })
+    });
+    $('.menuOpen__project').mouseleave(function () {
+        $('.menuSelect__project').css({
+            top: '-100%'
+        })
+    });
+    $('.menuOpen__contact').mouseenter(function () {
+        $('.menuSelect__contact').css({
+            top: 0
+        })
+    });
+    $('.menuOpen__contact').mouseleave(function () {
+        $('.menuSelect__contact').css({
+            top: '-100%'
+        })
+    });
 })
 function menuOpen() {
     $('.title').css({
@@ -14,94 +54,94 @@ function menuOpen() {
         right: '15%'
     });
     $('.menuOpen').css({
-        display : 'block'
+        display: 'block'
     })
-    setTimeout(function(){
+    setTimeout(function () {
         $('.menuOpen__home').css({
-             left: '25%',
+            left: '25%',
         })
-    },500)
-    setTimeout(function(){
+    }, 500)
+    setTimeout(function () {
         $('.menuOpen__about').css({
-             left: '37.5%',
+            left: '37.5%',
         })
-    },800)
-    setTimeout(function(){
+    }, 600)
+    setTimeout(function () {
         $('.menuOpen__project').css({
-             left: '50%',
+            left: '50%',
         })
-    },1100)
-    setTimeout(function(){
+    }, 700)
+    setTimeout(function () {
         $('.menuOpen__contact').css({
-             left: '62.5%',
+            left: '62.5%',
         })
-    },1400)
+    }, 800)
 }
 function start() {
     $(this).fadeOut();
     $('.area').animate({
-        height : '100vh'
-    },500);
-    setTimeout(function(){
+        height: '100vh'
+    }, 300);
+    setTimeout(function () {
         $('.area01').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },1000);
-    setTimeout(function(){
+    }, 600);
+    setTimeout(function () {
         $('.area02').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },1300);
-    setTimeout(function(){
+    }, 700);
+    setTimeout(function () {
         $('.area03').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },1600);
-    setTimeout(function(){
+    }, 800);
+    setTimeout(function () {
         $('.area04').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },1900);
-    setTimeout(function(){
+    }, 900);
+    setTimeout(function () {
         $('.area05').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },2200);
-    setTimeout(function(){
+    }, 1000);
+    setTimeout(function () {
         $('.area06').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },2500);
-    setTimeout(function(){
+    }, 1100);
+    setTimeout(function () {
         $('.area07').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },2800);
-    setTimeout(function(){
+    }, 1200);
+    setTimeout(function () {
         $('.area08').find('.area__wrapper').css({
-            width : '100%'
+            width: '100%'
         })
-    },3100);
-    setTimeout(function(){
+    }, 1300);
+    setTimeout(function () {
         $('.title').css({
-            display:'flex',
+            display: 'flex',
         })
-    },1300);
-    setTimeout(function(){
+    }, 600);
+    setTimeout(function () {
         $('.title').css({
             left: '15%'
         })
-    },3400);
-    setTimeout(function(){
+    }, 1300);
+    setTimeout(function () {
         $('.menu').css({
-            display:'flex',
+            display: 'flex',
         })
-    },3100);
-    setTimeout(function(){
+    }, 1400);
+    setTimeout(function () {
         $('.menu').css({
             right: '5%'
         })
-    },4000);
+    }, 1500);
     // setTimeout(function(){
     //     $('.circle01').css({
     //         border : '80px solid #ed924e'

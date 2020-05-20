27ac0321ca333dea4aaadc5e9e2ebf4d681d1077
lsh58 <tmdgusdl58@gmail.com>
2020-05-20 18:36:03 +0900

    ,

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function () {
     $('.menu').load("sub/menu.html");
     setTimeout(function () {
         start(); //시작이벤트 발동
-        $('.menu').click(menuToggle); //메뉴 열기&닫기
+        $('.menu__button').click(menuToggle); //메뉴 열기&닫기
         //메뉴 hover이벤트
         $('.area__wrapper').find('a').mouseenter(function () {
             $('.area__wrapper').find('a').css({
@@ -21,13 +21,13 @@ $(function () {
             });
         });
         //메뉴 hover이벤트 end
-    }, 10)
+    }, 100)
 });
-
+var bln = true;
 
 function menuToggle() {
-    //버튼 z-index 증가 및 x모양으로 변형
-    if ($('.menu__button').hasClass('open')) {
+    if ($('.menu__button').hasClass('open') && bln == false) {
+        //버튼 z-index 감소 및 원래모양으로 변형
         $('.menu__button').removeClass('open');
         $('.menu__button').css({
             zIndex: 100
@@ -44,8 +44,84 @@ function menuToggle() {
         $('.thirdSpan').css({
             transform: 'rotate(0) translateY(0)'
         });
+        //버튼 z-index 감소 및 원래모양으로 변형 end
+
+        //title 등장
+        setTimeout(function () {
+            $('.title').css({
+                display: 'flex',
+            })
+        }, 600);
+        setTimeout(function () {
+            $('.title').css({
+                left: '15%'
+            })
+        }, 1300);
+        //title 등장 end
+
+        //menu__background 사라지기
+        setTimeout(function () {
+            $('.home').css({
+                left: '-100%'
+            })
+        }, 100);
+        setTimeout(function () {
+            $('.about').css({
+                left: '-100%'
+            })
+        }, 200);
+        setTimeout(function () {
+            $('.project').css({
+                left: '-100%'
+            })
+        }, 300);
+        setTimeout(function () {
+            $('.contact').css({
+                left: '-100%'
+            })
+        }, 400);
+        setTimeout(function () {
+            for (var i = 1; i < 9; i++) {
+                var D = reverseDDDG(i, "menu__background");
+                D();
+            }
+        }, 100)
+        for (var i = 1; i < 9; i++) {
+            var D = DDDG(i, "background");
+            D();
+        }
+        $('.redArea').css({
+            width: 0
+        });
+        setTimeout(function () {
+            $('.area__wrapper').find('a').css({
+                display: 'none',
+            });
+        }, 600);
+        //menu__background 사라지기 end
+
+        //menu title 등장
+        $('.menu').find('h3').css({
+            top: '-5%'
+        });
+        //menu title 등장 end
+
+        //menu__background 원래위치로 돌려두기
+        setTimeout(function () {
+            $('.menu__background').find('.area__wrapper').css({
+                transform: 'translateX(0)',
+                width: 0
+            });
+            $('.menu__background').css({
+                zIndex: -1
+            });
+        }, 1600)
+        //menu__background 원래위치로 돌려두기;
+        setTimeout(function () {
+            bln = true;
+        }, 2500)
     }
-    else {
+    if (!$('.menu__button').hasClass('open') && bln == true) {
         $('.menu__button').addClass('open');
         $('.menu__button').css({
             zIndex: 100
@@ -63,6 +139,17 @@ function menuToggle() {
             transform: 'rotate(-45deg) translateY(-17px)'
         });
         //버튼 z-index 증가 및 x모양으로 변형 end
+
+        //title 숨기기
+        setTimeout(function () {
+            $('.title').css({
+                display: 'flex',
+            })
+        }, 300);
+        $('.title').css({
+            left: '-10%'
+        })
+        //title 숨기기 end
 
         //background 등장
         $('.menu__background').css({
@@ -119,8 +206,20 @@ function menuToggle() {
             $('.menu').find('h3').css({
                 top: '5%'
             });
-        }, 1500);
+        }, 2100);
         //menu title 등장 end
+
+        //background 원래위치로 돌려두기
+        setTimeout(function () {
+            $('.background').find('.area__wrapper').css({
+                transform: 'translateX(0)',
+                width: 0
+            })
+        }, 1600)
+        //background 원래위치로 돌려두기;
+        setTimeout(function () {
+            bln = false;
+        }, 2500)
     }
 }
 function start() {
